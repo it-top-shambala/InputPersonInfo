655480d76a006d6c3fbf478c1b30bef54302b52f
Dmitriy <dvol72@mail.ru>
2022-07-01 16:25:42 +0300

    Create new labels

--- InputPersonInfo.Lib/Person.cs
+++ InputPersonInfo.Lib/Person.cs
@@ -4,5 +4,9 @@ public class Person
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? Surname { get; set; }
     public int Age { get; set; }
+    public DateTime? BurthDay { get; set; }
+
+
 }
